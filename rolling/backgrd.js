a9228253a1e0e499960e79e69e26d6ce5ae2c5b3
poncho81 <paulo.jorente@web.de>
2018-02-24 13:16:34 +0100

    background changes

--- rolling/backgrd.js
+++ rolling/backgrd.js
@@ -2,19 +2,19 @@
 class Background {
     constructor() {
         this.width = 4 * SIZE;
-
+        this.roughness = .1;
         let b = document.createElement("canvas"),
             c = document.createElement("canvas"),
             d = document.createElement("canvas");
         
-        b.width = c.width  = d.width  = this.width;
+        b.width = SIZE; c.width  = d.width  = this.width;
         b.height = c.height = d.height = SIZE;
 
         let t0 = b.getContext("2d"),
             t1 = c.getContext("2d"),
             t2 = d.getContext("2d");
         
-        t1.fillStyle = "#2f0a7c";
+        t1.fillStyle = "#2a0578";
         t2.fillStyle = "#060225";
 
         this.create(t0);
@@ -86,25 +86,66 @@ class Background {
         }
     }
 
+    fillSqr(h, x1, y1, sz) {
+        if(sz < 1) return;
+
+        let x2 = x1 + sz, y2 = y1, x3 = x1, y3 = y1 + sz, x4 = x2, y4 = y3,
+            md = (sz >> 1),
+            disp = (Math.random() - .5) * (sz*2 / (SIZE * 2 * this.roughness)),
+			s = (h[x1][y1] + h[x2][y2] + h[x3][y3] + h[x4][y4]) / 4 + disp,
+			x = (x1 + x2) >> 1,
+			y = (y1 + y3) >> 1;
+
+		if(s < 0 ) s = 0;
+		else if(s > 1) s = 1;
+		h[x][y] = s;
+
+        h[x][y1] = (s + h[x1][y1] + h[x2][y2] + h[x ][(y1 - md + SIZE) % SIZE]) / 4;
+        h[x1][y] = (s + h[x1][y1] + h[x3][y3] + h[(x1 - md + SIZE) % SIZE][y ]) / 4;
+        h[x][y3] = (s + h[x4][y4] + h[x3][y3] + h[x ][(y3 + md + SIZE) % SIZE]) / 4;
+        h[x2][y] = (s + h[x4][y4] + h[x2][y2] + h[(x2 + md + SIZE) % SIZE][y ]) / 4;
+
+        sz = sz >> 1;
+		this.fillSqr(h, x1, y1, sz);
+		this.fillSqr(h,  x, y1, sz);
+		this.fillSqr(h, x1, y , sz);
+		this.fillSqr(h,  x, y , sz);
+	}
+
     create(cx) {
-        for(let g = 0; g < SIZE / TILE_SIZE; g++) {
-            for(let r = 0; r < this.width / TILE_SIZE; r++) {
-                let t = Math.floor(Math.random() * 256);
-                cx.fillStyle = `rgba(${t},${t},${t}, .12)`;
-                cx.fillRect(r * TILE_SIZE, g * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-                console.log(g);
-            }
-        }
+        let sz = 1 + Math.pow(2, Math.floor(Math.log2(SIZE) + .5));
+		let h = Array(sz);
+		for(let r = 0; r < sz; r++) {
+			h[r] = Array(sz);
+			h[r].fill(0);
+		}
+
+		sz--;
+		h[0][0] = h[sz][0] = h[0][sz] = h[sz][sz] = Math.random();
+		this.fillSqr(h, 0, 0, sz);
+
+		for(let r = 0; r < SIZE; r++) {
+			for(let c = 0; c < SIZE; c++) {
+				let t = (h[c][r] * 200) % 256;
+                cx.fillStyle = `rgba(${t}, ${t}, ${t}, .3)`;
+                let z = c;
+                for(let e = 0; e < 4; e++) {
+                    cx.fillRect(z, r, 1, 1);
+                    z += SIZE;
+                }
+			}
+		}
     }
 
     draw(ctx, p) {
         let pp = p / 8;
         this.x0 += pp;
-        if(this.x0 < -SIZE * 4) this.x0 = 0;
-
+        
+        if(this.x0 < -SIZE) this.x0 = 0;
         ctx.drawImage(this.img0, this.x0, 0);
-        if(this.x0 < SIZE * 3) {
-            ctx.drawImage(this.img0, this.x0 + SIZE * 4, 0);
+
+        if(this.x0 < 0) {
+            ctx.drawImage(this.img0, this.x0 + SIZE, 0);
         }
 
         pp = p / 4;
@@ -113,7 +154,7 @@ class Background {
 
         ctx.drawImage(this.img1, this.x1, 0);
         if(this.x1 < SIZE * 3) {
-            ctx.drawImage(this.img1, this.x1 + SIZE * 4, 0);
+            ctx.drawImage(this.img1, this.x1 + SIZE * 4 - 1, 0);
         }
 
         pp = p / 2;
@@ -122,7 +163,7 @@ class Background {
 
         ctx.drawImage(this.img2, this.x2, 0);
         if(this.x2 < SIZE * 3) {
-            ctx.drawImage(this.img2, this.x2 + SIZE * 4, 0);
+            ctx.drawImage(this.img2, this.x2 + SIZE * 4 - 1, 0);
         }
     } 
 }
